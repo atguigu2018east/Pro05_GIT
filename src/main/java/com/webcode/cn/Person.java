334813d035e5b78ccaeb457456dafbc166d2da74
shao <pei@.com>
2018-11-12 12:44:37 +0800

    添加一些文件

---
+++ src/main/java/com/webcode/cn/Person.java
@@ -0,0 +1,13 @@
+package com.webcode.cn;
+
+public class Person {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("======");
+		System.out.println("aaaa");
+		System.out.println("我心依旧");//ddd
+		System.out.println("我心依旧");//ddAdsdfgh
+		System.out.println("wo");
+	}
+}
---
+++ src/test/java/com/webcode/cn/PersonTest.java
@@ -0,0 +1,5 @@
+package com.webcode.cn;
+
+public class PersonTest {
+
+}
